f888c666ca35264de6f55ed7fd74c5db16a7c6d5
tsiozos <38419287+tsiozos@users.noreply.github.com>
2021-02-02 00:10:47 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,89 @@
+
+class Options:
+    PASTPACKETSLENGTH = 100
+    RADIOBUFFERLENGTH = 16
+    REGISTEREDSENSORSLENGTH = 16
+
+class MySensor:    #assign id's to onboard sensors
+    STATION = 0
+    LIGHT=1
+    COMPASS=2
+    TEMPER=3
+    ROLL=4
+    PITCH=5
+    ACCX=6
+    ACCY=7
+    ACCZ=8
+    ROTX=9
+    ROTY=10
+    ROTZ=11
+    MAGX=12
+    MAGY=13
+    MAGZ=14
+
+#class SensorFuncs:
+#    @staticmethod
+def getLight():
+    return input.light_level()
+#    @staticmethod
+def getCompass():
+    return input.compass_heading()
+#    @staticmethod
+def getTemp():
+    return input.temperature()
+
+def getRoll():
+    return input.rotation(Rotation.ROLL)
+
+def getPitch():
+    return input.rotation(Rotation.PITCH)
+
+def getAccX():
+    return input.acceleration(Dimension.X)
+
+def getAccY():    
+    return input.acceleration(Dimension.Y)
+
+def getAccZ():    
+    return input.acceleration(Dimension.Z)
+
+def getAccStrength():    
+    return input.acceleration(Dimension.STRENGTH)
+
+def getMagX():
+    return input.magnetic_force(Dimension.X) 
+
+def getMagY():
+    return input.magnetic_force(Dimension.Y)
+
+def getMagZ():
+    return input.magnetic_force(Dimension.Z)
+
+def SensorError():
+    print("ERROR Sensor Code")
+    return 0
+
+def MySensorFunc(func):
+    if func == MySensor.LIGHT: return getLight()
+    elif func == MySensor.COMPASS: return getCompass()
+    elif func == MySensor.TEMPER: return getTemp()
+    elif func == MySensor.ROLL: return getRoll()
+    elif func == MySensor.PITCH: return getPitch()
+    elif func == MySensor.ACCX: return getAccX()
+    elif func == MySensor.ACCY: return getAccY()
+    elif func == MySensor.ACCZ: return getAccZ()
+    elif func == MySensor.MAGX: return getMagX()
+    elif func == MySensor.MAGY: return getMagY()
+    elif func == MySensor.MAGZ: return getMagZ()
+    else: return SensorError()
+
+#def boolarrayToHex(arr):    #static methods do not use 'self' parameter
+#    tt = map(int, arr)
+#    n = int(''.join(map(str,tt)),2)
+#    return '{:02x}'.format(n).lstrip()
+
+#print(str(Options.PASTPACKETSLENGTH))
+#print(str(Options.RADIOBUFFERLENGTH))
+#print(str(Options.REGISTEREDSENSORSLENGTH))
+print(str(MySensorFunc(MySensor.TEMPER)))
+
---
+++ main.ts
@@ -0,0 +1,119 @@
+class Options {
+    static PASTPACKETSLENGTH = 100
+    static RADIOBUFFERLENGTH = 16
+    static REGISTEREDSENSORSLENGTH = 16
+}
+
+class MySensor {
+    // assign id's to onboard sensors
+    static STATION = 0
+    static LIGHT = 1
+    static COMPASS = 2
+    static TEMPER = 3
+    static ROLL = 4
+    static PITCH = 5
+    static ACCX = 6
+    static ACCY = 7
+    static ACCZ = 8
+    static ROTX = 9
+    static ROTY = 10
+    static ROTZ = 11
+    static MAGX = 12
+    static MAGY = 13
+    static MAGZ = 14
+}
+
+// class SensorFuncs:
+//     @staticmethod
+function getLight(): number {
+    return input.lightLevel()
+}
+
+//     @staticmethod
+function getCompass(): number {
+    return input.compassHeading()
+}
+
+//     @staticmethod
+function getTemp(): number {
+    return input.temperature()
+}
+
+function getRoll(): number {
+    return input.rotation(Rotation.Roll)
+}
+
+function getPitch(): number {
+    return input.rotation(Rotation.Pitch)
+}
+
+function getAccX(): number {
+    return input.acceleration(Dimension.X)
+}
+
+function getAccY(): number {
+    return input.acceleration(Dimension.Y)
+}
+
+function getAccZ(): number {
+    return input.acceleration(Dimension.Z)
+}
+
+function getAccStrength(): number {
+    return input.acceleration(Dimension.Strength)
+}
+
+function getMagX(): number {
+    return input.magneticForce(Dimension.X)
+}
+
+function getMagY(): number {
+    return input.magneticForce(Dimension.Y)
+}
+
+function getMagZ(): number {
+    return input.magneticForce(Dimension.Z)
+}
+
+function SensorError(): number {
+    console.log("ERROR Sensor Code")
+    return 0
+}
+
+function MySensorFunc(func: number): number {
+    if (func == MySensor.LIGHT) {
+        return getLight()
+    } else if (func == MySensor.COMPASS) {
+        return getCompass()
+    } else if (func == MySensor.TEMPER) {
+        return getTemp()
+    } else if (func == MySensor.ROLL) {
+        return getRoll()
+    } else if (func == MySensor.PITCH) {
+        return getPitch()
+    } else if (func == MySensor.ACCX) {
+        return getAccX()
+    } else if (func == MySensor.ACCY) {
+        return getAccY()
+    } else if (func == MySensor.ACCZ) {
+        return getAccZ()
+    } else if (func == MySensor.MAGX) {
+        return getMagX()
+    } else if (func == MySensor.MAGY) {
+        return getMagY()
+    } else if (func == MySensor.MAGZ) {
+        return getMagZ()
+    } else {
+        return SensorError()
+    }
+    
+}
+
+// def boolarrayToHex(arr):    #static methods do not use 'self' parameter
+//     tt = map(int, arr)
+//     n = int(''.join(map(str,tt)),2)
+//     return '{:02x}'.format(n).lstrip()
+// print(str(Options.PASTPACKETSLENGTH))
+// print(str(Options.RADIOBUFFERLENGTH))
+// print(str(Options.REGISTEREDSENSORSLENGTH))
+console.log("" + MySensorFunc(MySensor.TEMPER))
